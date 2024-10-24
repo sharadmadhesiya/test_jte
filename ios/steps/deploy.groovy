708c8e9d434256ac9f594d9238cfa53188d258a5
sharadmadhesiya <sharad.madhesiya@statusneo.com>
2024-10-24 11:40:40 +0530

    Update deploy.groovy

--- ios/steps/deploy.groovy
+++ ios/steps/deploy.groovy
@@ -4,16 +4,16 @@ void call() {
         println "Test ios library"
             println("#################### STARTED S3  DEPLOYMENT ####################")
         println(env.APPSTORE_KEY_ID )
-    if (env.APPSTORE_KEY_ID != null ) {
-      withCredentials([[ credentialsId: "${env.APPSTORE_KEY_ID}"]]) {
+    // if (env.APPSTORE_KEY_ID != null ) {
+      withCredentials([[ credentialsId: "${env.APPSTORE_KEY_ID}", variable: 'APPSTORE_KEY_ID']]) {
           sh """
-          echo ${env.APPSTORE_KEY_ID}
+          echo ${APPSTORE_KEY_ID}
           """
       		
         }
-    }
-    else{
-      println("Missing required Application environment parameters. Please check source code pipeline_config.groovy file variables: S3_BUCKET_NAME,AWS_CREDENTIALS_ID, DEPLOY_LIB_BRANCH_NAME, DEPLOY_TARGET_FOLDER")   
-    }
+    //}
+    // else{
+    //   println("Missing required Application environment parameters. Please check source code pipeline_config.groovy file variables: S3_BUCKET_NAME,AWS_CREDENTIALS_ID, DEPLOY_LIB_BRANCH_NAME, DEPLOY_TARGET_FOLDER")   
+    // }
     }
 }
