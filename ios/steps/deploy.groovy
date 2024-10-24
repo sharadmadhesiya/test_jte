a2db6ce1b944cfae8e0924d31e9e424102633047
sharadmadhesiya <sharad.madhesiya@statusneo.com>
2024-10-24 10:59:06 +0530

    added env variables

---
+++ ios/steps/deploy.groovy
@@ -0,0 +1,14 @@
+void call() {
+    logger.info("#################### STARTED S3  DEPLOYMENT ####################")
+    if (env.APPSTORE_KEY_ID != null ) {
+      withCredentials([[ credentialsId: "${env.APPSTORE_KEY_ID}"]]) {
+          sh """
+          echo ${env.APPSTORE_KEY_ID}
+          """
+      		
+        }
+    }
+    else{
+      logger.error("Missing required Application environment parameters. Please check source code pipeline_config.groovy file variables: S3_BUCKET_NAME,AWS_CREDENTIALS_ID, DEPLOY_LIB_BRANCH_NAME, DEPLOY_TARGET_FOLDER")   
+    }
+}
