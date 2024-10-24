27e58bf2643b4cc0c68e0a013c136e0ab7a50c77
sharadmadhesiya <sharad.madhesiya@statusneo.com>
2024-10-24 11:50:14 +0530

    Update deploy.groovy

--- ios/steps/deploy.groovy
+++ ios/steps/deploy.groovy
@@ -1,13 +1,16 @@
-void call() {
-    stage("Test: Static Code Analysis") {
-        println "Test ios library"
-        println("#################### STARTED S3 DEPLOYMENT ####################")
 
-        // Ensure APPSTORE_KEY_ID is defined in Jenkins Credentials
-        withCredentials([string(credentialsId: 'APPSTORE_KEY_ID', variable: 'APPSTORE_KEY_ID')]) {
-            sh '''
-            echo $APPSTORE_KEY_ID
-            '''
+void call() {
+    node {
+        stage("Test: Static Code Analysis") {
+            println "Test ios library"
+            println("#################### STARTED S3 DEPLOYMENT ####################")
+
+            // Ensure APPSTORE_KEY_ID is defined in Jenkins Credentials
+            withCredentials([string(credentialsId: 'APPSTORE_KEY_ID', variable: 'APPSTORE_KEY_ID')]) {
+                sh '''
+                echo $APPSTORE_KEY_ID
+                '''
+            }
         }
     }
 }
