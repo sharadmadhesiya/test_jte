9e19e6f2ca877cbcad52c86e0dfd875f713066e0
sharadmadhesiya <sharad.madhesiya@statusneo.com>
2024-10-24 16:34:45 +0530

    Update deploy.groovy

--- ios/steps/deploy.groovy
+++ ios/steps/deploy.groovy
@@ -6,7 +6,7 @@ void call(app_env) {
             println(pipelineConfig.APPSTORE_BUNDLE_ID)
             //println (pipelineConfig.APPSTORE_BUNDLE_ID)
             
-            println("#################### STARTED S3 DEPLOYMENT ####################")
+            println("#################### STARTED ios DEPLOYMENT ####################")
 
             // Print the APPSTORE_BUNDLE_ID in Groovy
             //println("App Store Bundle ID: ${APPSTORE_BUNDLE_ID}")
@@ -14,6 +14,7 @@ void call(app_env) {
             // Ensure APPSTORE_KEY_ID is defined in Jenkins Credentials
             withCredentials([string(credentialsId: 'APPSTORE_KEY_ID', variable: 'APPSTORE_KEY_ID')]) {
                 sh """
+                ls -lh
                 echo \$APPSTORE_KEY_ID
                 echo "Checking bundle id from shell"
                 echo $pipelineConfig.APPSTORE_BUNDLE_ID
