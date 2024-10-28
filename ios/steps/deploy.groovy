9acff5537fb08cfa70ed19ea2fbede980938ab37
sharadmadhesiya <sharad.madhesiya@statusneo.com>
2024-10-28 12:28:44 +0530

    test

--- ios/steps/deploy.groovy
+++ ios/steps/deploy.groovy
@@ -1,29 +1,33 @@
 void call(app_env) {
     node {
         stage("Test: Static Code Analysis") {
-            println "Test ios library"
+            println "Test iOS library"
             println(pipelineConfig)
-            println("#################### STARTED ios DEPLOYMENT ####################")
+            println("#################### STARTED iOS DEPLOYMENT ####################")
 
             withCredentials([string(credentialsId: 'APPSTORE_KEY_ID', variable: 'APPSTORE_KEY_ID')]) {
                 sh """
+                # Clone or pull the latest code
                 if [ -d "jte_pipeline" ]; then
-                  cd jte_pipeline && git pull
+                    cd jte_pipeline && git pull
                 else
-                  git clone https://github.com/sharadmadhesiya/jte_pipeline.git
+                    git clone https://github.com/sharadmadhesiya/jte_pipeline.git
+                    cd jte_pipeline
                 fi
 
+                # Display current user and check key ID
                 ls -lh
                 echo \$APPSTORE_KEY_ID
                 echo "Checking bundle id from shell"
-                echo $pipelineConfig.APPSTORE_BUNDLE_ID
+                echo ${pipelineConfig.APPSTORE_BUNDLE_ID}
                 whoami
+
                 # Check if rbenv is installed, if not, install it
                 if ! command -v rbenv &> /dev/null; then
                     echo "rbenv could not be found, installing..."
                     brew install rbenv
-                    eval "$(rbenv init -)"
                 fi
+                eval "$(rbenv init -)"
 
                 # Install Ruby version and set it up
                 rbenv install 3.0.0 --skip-existing
@@ -35,52 +39,16 @@ void call(app_env) {
                 export GEM_PATH=$GEM_HOME
                 export PATH=$GEM_HOME/bin:$PATH
 
-                # Install the specified version of bundler
+                # Install the specified version of Bundler and bundle install
                 gem install bundler -v 2.4.22 --user-install
-
-                
+                cd jte_pipeline  # Navigate to directory with Gemfile, if not already there
                 bundle install
 
-                xcode-select --switch /Applications/Xcode.app/Contents/Developer
+                # Set up and verify Xcode configuration
+                sudo xcode-select --switch /Applications/Xcode.app/Contents/Developer
                 xcodebuild -version
-
                 """
             }
         }
-
-//         stage('Install Dependencies') {
-//         sh '''
-//         # Check if rbenv is installed, if not, install it
-//         if ! command -v rbenv &> /dev/null; then
-//             echo "rbenv could not be found, installing..."
-//             brew install rbenv
-//             eval "$(rbenv init -)"
-//         fi
-
-//         # Install Ruby version and set it up
-//         rbenv install 3.0.0 --skip-existing
-//         rbenv global 3.0.0
-//         eval "$(rbenv init -)"
-
-//         # Set up GEM_HOME, GEM_PATH, and PATH
-//         export GEM_HOME=$(rbenv root)/versions/3.0.0
-//         export GEM_PATH=$GEM_HOME
-//         export PATH=$GEM_HOME/bin:$PATH
-
-//         # Install the specified version of bundler
-//         gem install bundler -v 2.4.22 --user-install
-
-        
-//         bundle install
-//         '''
-// }
-
-
-        // stage('Configure Xcode') {
-        //     sh '''
-        //     sudo xcode-select --switch /Applications/Xcode.app/Contents/Developer
-        //     xcodebuild -version
-        //     '''
-        // }
     }
 }
