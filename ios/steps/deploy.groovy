c484ee9520bb5b6fb84e2c8c515e58311ce1d7fd
sharadmadhesiya <sharad.madhesiya@statusneo.com>
2024-10-29 12:51:50 +0530

    Update deploy.groovy

--- ios/steps/deploy.groovy
+++ ios/steps/deploy.groovy
@@ -6,19 +6,10 @@ void call(app_env) {
             println("#################### STARTED iOS DEPLOYMENT ####################")
 
             withCredentials([string(credentialsId: 'APPSTORE_KEY_ID', variable: 'APPSTORE_KEY_ID'),
-            string(credentialsId: 'KEYCHAINPASSWORD', variable: 'KEYCHAINPASSWORD'),
-            string(credentialsId: 'APPSTORE_API_KEY_FILE', variable: 'APPSTORE_API_KEY_FILE'),
-            string(credentialsId: 'GITAUTHORIZATION', variable: 'GITAUTHORIZATION'),
-            string(credentialsId: 'MATCH_PASSWORD', variable: 'MATCH_PASSWORD'),\
-
-            ]) {
-
-                script {
-                    env.key_id = "\$APPSTORE_KEY_ID"
-                    // Iterate over all environment variables and print them
-                    env.each { key, value ->
-                        echo "${key}=${value}"
-                }
+                             string(credentialsId: 'KEYCHAINPASSWORD', variable: 'KEYCHAINPASSWORD'),
+                             string(credentialsId: 'APPSTORE_API_KEY_FILE', variable: 'APPSTORE_API_KEY_FILE'),
+                             string(credentialsId: 'GITAUTHORIZATION', variable: 'GITAUTHORIZATION'),
+                             string(credentialsId: 'MATCH_PASSWORD', variable: 'MATCH_PASSWORD')]) {
 
                 script {
                     // Set environment variables
@@ -29,60 +20,53 @@ void call(app_env) {
                     
                     // Run Fastlane with the environment variables
                     sh """
-                    /${envVars}
-                    echo "/$MY_VAR"
+                    ${envVars}
+                    echo "MY_VAR: ${MY_VAR}"
+                    
                     if [ -d "jte_pipeline" ]; then
                         cd jte_pipeline && git pull
                     else
                         git clone https://github.com/sharadmadhesiya/jte_pipeline.git
                         cd jte_pipeline
                     fi
-    
+
                     # Display current user and check key ID
                     ls -lh
-    
-                    echo "Checking bundle id from shell"
-                    
-                    whoami
-    
-                    echo "\$APPSTORE_KEY_ID"
-                    
-                    
-    
+                    echo "Current user: \$(whoami)"
+                    echo "App Store Key ID: \$APPSTORE_KEY_ID"
+
                     # Check if rbenv is installed, if not, install it
                     if ! command -v rbenv &> /dev/null; then
                         echo "rbenv could not be found, installing..."
                         brew install rbenv
                     fi
                     eval "\$(rbenv init -)"
-    
+
                     # Install Ruby version and set it up
                     rbenv install 3.0.0 --skip-existing
                     rbenv global 3.0.0
                     eval "\$(rbenv init -)"
-    
+
                     # Set up GEM_HOME, GEM_PATH, and PATH
                     export GEM_HOME=\$(rbenv root)/versions/3.0.0
                     export GEM_PATH=\$GEM_HOME
                     export PATH=\$GEM_HOME/bin:\$PATH
-    
+
                     # Install the specified version of Bundler and bundle install
                     gem install bundler -v 2.4.22 --user-install
                     bundle install
-    
+
                     # Set up and verify Xcode configuration
                     sudo xcode-select --switch /Applications/Xcode.app/Contents/Developer
                     xcodebuild -version
-    
+
                     echo \$APPSTORE_API_KEY_FILE >> "fastlane/AuthKey_file.p8"
                     ls fastlane
                     cat fastlane/AuthKey_file.p8
                     bundle exec fastlane release_build --verbose
                     """
                 }
-                
             }
         }
     }
-}
 }
